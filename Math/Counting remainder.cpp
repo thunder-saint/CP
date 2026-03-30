1373a7ba48ab6b49867f3a1aa7490be33d6a6fb6
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-03-30 20:12:50 +0600

    Update Counting remainder.cpp

--- Math/Counting remainder.cpp
+++ Math/Counting remainder.cpp
@@ -1,5 +1,5 @@
 // Returns the count of non-negative integers p in the range [0, x] such that (p % n == rem).
 int count_rem(int x, int rem, int n) {
-    if (x < rem) return 0;
+    if (rem >= n || x < rem) return 0;
     return (x - rem) / n + 1;
 }
