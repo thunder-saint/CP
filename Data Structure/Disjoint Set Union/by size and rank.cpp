96af863a6fcdfd5ae61b37992c59872f93ad4b96
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-04-27 00:07:47 +0600

    Refactor DSU structure and methods for clarity

--- Data Structure/Disjoint Set Union/by size and rank.cpp
+++ Data Structure/Disjoint Set Union/by size and rank.cpp
@@ -1,45 +1,33 @@
 struct DSU {
-    vector<int> pars, sz, parr, rnk;
+    vector<int> par, sz, rnk;
     int components;
     DSU(int n) {
         components = n;
-        pars.resize(n + 1);
+        par.resize(n + 1);
         sz.assign(n + 1, 1);
-        iota(pars.begin(), pars.end(), 0);
-
-        parr.resize(n + 1);
+        iota(par.begin(), par.end(), 0);
         rnk.assign(n + 1, 0);
-        iota(parr.begin(), parr.end(), 0);
     }
-    int finds(int x) {
-        while (x != pars[x]) {
-            pars[x] = pars[pars[x]]; 
-            x = pars[x];
+    int find(int x) {
+        while (x != par[x]) {
+            par[x] = par[par[x]]; 
+            x = par[x];
         }
         return x;
     }
-    void unions(int u, int v) {
-        int ru = finds(u), rv = finds(v);
+    void Union (int u, int v) {
+        int ru = find(u), rv = find(v);
         if (ru == rv) return;
-        if (sz[ru] < sz[rv]) swap(ru, rv);
-        pars[rv] = ru;
-        sz[ru] += sz[rv];
         components--;
+        // by size
+        if (sz[ru] < sz[rv]) swap(ru, rv);
+        par[rv] = ru;
+        sz[ru] += sz[rv];
+        
+        // by rank
+        // if (rnk[ru] < rnk[rv]) swap(ru, rv);
+        // if (rnk[ru] == rnk[rv]) rnk[ru]++;
+        // par[rv] = ru;
     }
 
-    int findr(int x) {
-        while (x != parr[x]) {
-            parr[x] = parr[parr[x]]; 
-            x = parr[x];
-        }
-        return x;
-    }
-    void unionr(int u, int v) {
-        int ru = findr(u), rv = findr(v);
-        if (ru == rv) return;
-        if (rnk[ru] < rnk[rv]) swap(ru, rv);
-        if (rnk[ru] == rnk[rv]) rnk[ru]++;
-        parr[rv] = ru;
-        components--;
-    }
 };
