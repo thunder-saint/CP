b88c1779e757cc30b5c4e031cf1155db24aeacef
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-04-28 01:03:28 +0600

    Update Dijkstra.cpp

--- Graph Theory/Shortest Path Algorithms/Dijkstra.cpp
+++ Graph Theory/Shortest Path Algorithms/Dijkstra.cpp
@@ -1,19 +1,19 @@
 #define pi pair<int,int>
 const int N = 1e5 + 1;
+const int inf = 1e18;
 vector <pi> g[N];
 int dist[N], par[N], cnt[N], mn[N], mx[N];
 void dijkstra(int source, int n) {
     priority_queue<pi,vector<pi>,greater<pi>>pq;
     for(int i=1; i<=n; i++) {
-        dist[i] = mn[i] = LLONG_MAX;
+        dist[i] = mn[i] = inf;
         cnt[i] = par[i] = mx[i] = 0;
     }
     dist[source] = mn[source] = mx[source] = 0;
     cnt[source] = 1;
     pq.push({0, source});
     while(!pq.empty()) {
-        auto [d,u] = pq.top(); 
-        pq.pop();
+        auto [d,u] = pq.top(); pq.pop();
         if(d > dist[u]) continue;
         for(auto &[v,w] : g[u]) {
             if(dist[u] + w < dist[v]) {
@@ -24,29 +24,26 @@ void dijkstra(int source, int n) {
         }
     } 
     // extra
-    vector<vector<int>> dag(n + 1);
     vector<int> in(n + 1, 0);
      for (int u = 1; u <= n; u++) {
-        if (dist[u] == LLONG_MAX) continue;
+        if (dist[u] == inf) continue;
         for (auto &[v, w] : g[u]) {
-            if (dist[u] + w == dist[v]) {
-                dag[u].push_back(v), in[v]++;
-            }
+            if (dist[u] + w == dist[v]) in[v]++;
         }
     }
     queue<int> q;
     for (int i = 1; i <= n; i++) {
-        if (dist[i] < LLONG_MAX && !in[i]) q.push(i);
+        if (dist[i] < inf && !in[i]) q.push(i);
     }
     while (!q.empty()) {
         int u = q.front(); q.pop();
-        for (int &v : dag[u]) {
-            if (cnt[u] && mn[u] != LLONG_MAX) { 
+        for (auto &[v,w] : g[u]) {
+            if (dist[u] + w == dist[v]) { 
                 cnt[v] = (cnt[v] + cnt[u]) % MOD;
                 mn[v]  = min(mn[v], mn[u] + 1);
                 mx[v]  = max(mx[v], mx[u] + 1);
+                if (--in[v] == 0) q.push(v);
             }
-            if (--in[v] == 0) q.push(v);
         }
     }
 }
