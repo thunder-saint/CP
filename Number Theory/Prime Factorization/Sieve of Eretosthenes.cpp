71ef4ca6ac4ea2c3c2830a5e4db62e8741317164
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-05-24 14:58:44 +0600

    Simplify inner loop in Sieve of Eratosthenes

--- Number Theory/Prime Factorization/Sieve of Eretosthenes.cpp
+++ Number Theory/Prime Factorization/Sieve of Eretosthenes.cpp
@@ -6,9 +6,7 @@ void sieve() {
     prime[0] = prime[1] = 0;
     for (int i = 2; i * i <= MAX_N; i++) {
         if (prime[i]) {
-            for (int j = i * i; j <= MAX_N; j += i) {
-                prime[j] = 0;
-            }
+            for (int j = i * i; j <= MAX_N; j += i) prime[j] = 0;
         }
     }
     for(int i=2; i<= MAX_N; i++) {
