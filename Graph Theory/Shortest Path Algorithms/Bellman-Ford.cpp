f80188fe4dcee4bfbdc616d78ccf37ee10614477
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-05-26 22:29:10 +0600

    Update Bellman-Ford.cpp

--- Graph Theory/Shortest Path Algorithms/Bellman-Ford.cpp
+++ Graph Theory/Shortest Path Algorithms/Bellman-Ford.cpp
@@ -1,33 +1,31 @@
-const int N = 25001 + 123;
-const int inf = 1e18;
-vector<tuple<int, int, int>> e;
-int d[N], p[N]; 
-bool Neg;
-vector<int> nc;
+const int INF = 1e18;
+vector <tuple<int,int,int>> e;
+vector<long long> d, p, nc; 
+bool neg = false;
 void bellman_ford(int s, int n, bool fl) {
-    for (int i = 1; i <= n; i++) d[i] = fl? 0 : inf, p[i] = -1;
-    d[s] = 0, p[s] = s, Neg = false, nc.clear();;
-    for (int i = 1; i <= n; i++) {
-        bool updated = false;
-        for (auto &[u, v, w] : e) {
-            if (d[u] < inf && d[u] + w < d[v]) {
-                d[v] = d[u] + w, p[v] = u, updated = true;           
+    d.assign(n + 1, fl ? 0 : INF);
+    p.assign(n + 1, -1), nc.clear();
+    neg = false, d[s] = 0; 
+    int l = -1;
+    for (int i = 1; i <= n; ++i) {
+        l = -1;
+        for (auto &[u, v, w]  : e) {
+            if (d[u] < INF && d[u] + w < d[v]) {
+                d[v] = d[u] + w, p[v] = u, l = v;
             }
         }
-        if (!updated) break; 
+        if (l == -1) return;
     }
-    int x = -1;
-    for (int i = 1; i <= n; i++) {
-        for (auto &[u, v, w] : e) {
-            if (d[u] < inf && (d[u] == -inf || d[u] + w < d[v]) && d[v] != -inf){
-                d[v] = -inf, p[v] = u, x = v, Neg = true;
+    neg = true;
+    for(int i = 1; i <= n; i++) l = p[l];
+    nc.push_back(l); int c = p[l];
+    while(c != l) nc.push_back(c), c = p[c];
+    nc.push_back(l), reverse(all(nc));
+    for (int i = 1; i <= n; ++i) {
+        for (auto &[u,v,w] : e) {
+            if (d[u] < INF) {
+                if (d[u] == -INF || d[u] + w < d[v]) d[v] = -INF;
             }
         }
     }
-    if(x == -1) return;
-    for(int i = 1; i <= n; i++) x = p[x];
-    nc.push_back(x);
-    int c = p[x];
-    while(c != x) nc.push_back(c), c = p[c];
-    nc.push_back(x), reverse(all(nc));
 }
