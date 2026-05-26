3fe7ccb774ca76dc2ca09ddd97ae3b279210843f
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-05-26 16:38:11 +0600

    Refactor Bellman-Ford algorithm and fix negative cycle detection

--- Graph Theory/Shortest Path Algorithms/Bellman-Ford.cpp
+++ Graph Theory/Shortest Path Algorithms/Bellman-Ford.cpp
@@ -2,26 +2,32 @@ const int N = 1e3 + 123;
 const int inf = 1e18;
 vector<tuple<int, int, int>> e;
 int d[N], p[N]; 
-bool NegativeCycle;
+bool Neg;
+vector<int> nc;
 void bellman_ford(int s, int n) {
-    NegativeCycle = false;
     for (int i = 1; i <= n; i++) d[i] = inf, p[i] = -1;
-    d[s] = 0;
-    for (int i = 1; i < n; i++) {
+    d[s] = 0, p[s] = s, Neg = false, nc.clear();;
+    for (int i = 1; i <= n; i++) {
         bool updated = false;
         for (auto &[u, v, w] : e) {
             if (d[u] < inf && d[u] + w < d[v]) {
-                d[v] = max(-inf, d[u] + w);
-                p[v] = u, updated = true;           
+                d[v] = d[u] + w, p[v] = u, updated = true;           
             }
         }
-        if (!updated) return; 
+        if (!updated) break; 
     }
-    for (int i = 1; i <= n; i++) {
+    int x = -1;
+    for (int i = 1; i < n; i++) {
         for (auto &[u, v, w] : e) {
-            if (d[u] < inf && (d[u] == -inf || d[u] + w < d[v]) && d[v] != -inf) {
-                d[v] = -inf, NegativeCycle = true;
+            if (d[u] < inf && (d[u] == -inf || d[u] + w < d[v])){
+                d[v] = -inf, x = v, Neg = true;
             }
         }
     }
+    if(x == -1) return;
+    for(int i = 1; i <= n; i++) x = p[x];
+    nc.push_back(x);
+    int c = p[x];
+    while(c != x) nc.push_back(c), c = p[c];
+    nc.push_back(x), reverse(all(nc));
 }
