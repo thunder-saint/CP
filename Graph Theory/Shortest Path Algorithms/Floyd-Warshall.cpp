5eb55b995f6dc81198b536d489e53671bab5f40c
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-06-11 20:38:22 +0600

    Update Floyd-Warshall.cpp

--- Graph Theory/Shortest Path Algorithms/Floyd-Warshall.cpp
+++ Graph Theory/Shortest Path Algorithms/Floyd-Warshall.cpp
@@ -1,10 +1,10 @@
-const int INF = 1e18;         
+const int inf = 1e18;         
 const int N   = 505;
 int d[N][N], nxt[N][N], par[N][N]; 
 void init_graph(int n) {
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= n; ++j) {
-            d[i][j] = (i == j) ? 0 : INF;
+            d[i][j] = (i == j) ? 0 : inf;
             nxt[i][j] = (i == j) ? i : -1;
             par[i][j] = (i == j) ? i : -1;
         }
@@ -13,9 +13,9 @@ void init_graph(int n) {
 void floyd_warshall(int n) {
     for (int k = 1; k <= n; ++k) {
         for (int i = 1; i <= n; ++i) {
-            if (d[i][k] == INF) continue;
+            if (d[i][k] == inf) continue;
             for (int j = 1; j <= n; ++j) {
-                if (d[k][j] == INF) continue;
+                if (d[k][j] == inf) continue;
                 if (d[i][k] + d[k][j] < d[i][j]) {
                     d[i][j] = d[i][k] + d[k][j];
                     nxt[i][j] = nxt[i][k];
@@ -28,8 +28,8 @@ void floyd_warshall(int n) {
         if (d[k][k] >= 0) continue;                 
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= n; ++j) {
-                if (d[i][k] < INF && d[k][j] < INF) {
-                    d[i][j] = -INF;
+                if (d[i][k] < inf && d[k][j] < inf) {
+                    d[i][j] = -inf;
                 }
             }
         }   
