6b14e925b0257cbc49082a84c63b5c05d7c323a5
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-06-19 15:24:26 +0600

    Update Binary Exponentiation.cpp

--- Number Theory/Modular Arithmetic/Binary Exponentiation.cpp
+++ Number Theory/Modular Arithmetic/Binary Exponentiation.cpp
@@ -3,9 +3,15 @@ int power(int x, int n, int MOD) {
     x %= MOD;
     if (x < 0) x += MOD;
     while (n) {
-        if (n & 1) ans =  1LL * ans * x % MOD;
-        x = 1LL * x * x % MOD;
+        if (n & 1) ans = (int)((__int128)ans * x % MOD); 
+        x = (int)((__int128)x * x % MOD);
         n >>= 1; 
     }
     return ans;
+}
+int Inverse(int n, int MOD) {
+    n %= MOD;
+    if (n < 0) n += MOD;
+    if (n == 0) return -1;
+    return power(n, MOD - 2, MOD);
 }
