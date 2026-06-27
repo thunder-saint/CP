2409070e499caa46e4c6c64404b66c723e04adc4
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-06-27 12:58:59 +0600

    Update Sieve X SPF X Phi X Möbius.cpp

--- Number Theory/Sieve X SPF X Phi X Möbius.cpp
+++ Number Theory/Sieve X SPF X Phi X Möbius.cpp
@@ -1,19 +1,19 @@
 const int32_t MX = 1e8, N = 1e7;
 bitset<MX + 1> isprime;
 int32_t spf[N + 1], phi[N + 1];
-int32_t div[N + 1], pw[N + 1];
+int32_t dv[N + 1], pw[N + 1];
 int8_t mu[N + 1];
 vector<int32_t> primes;
 void sieve() {
     primes.reserve(5761455); 
-    isprime.set(), phi[1] = mu[1] = div[1] = 1;
+    isprime.set(), phi[1] = mu[1] = dv[1] = 1;
     isprime[0] = isprime[1] = 0;
     for (int i = 2; i <= MX; i++) {
         if (isprime[i]) {
             primes.push_back(i);
             if (i <= N) {
                 spf[i] = i, phi[i] = i - 1;
-                mu[i] = -1, div[i] = 2, pw[i] = 1;
+                mu[i] = -1, dv[i] = 2, pw[i] = 1;
             }
         }
         for (int j = 0; j < primes.size(); j++) {
@@ -25,12 +25,12 @@ void sieve() {
                if (i % primes[j] == 0) {
                     phi[nxt] = phi[i] * primes[j];
                     mu[nxt] = 0, pw[nxt] = pw[i] + 1;
-                    div[nxt] = div[i] / (pw[i] + 1) * (pw[i] + 2);
+                    dv[nxt] = dv[i] / (pw[i] + 1) * (pw[i] + 2);
                 } 
                 else {
                     phi[nxt] = phi[i] * (primes[j] - 1);
                     mu[nxt] = -mu[i], pw[nxt] = 1; 
-                    div[nxt] = div[i] * 2;
+                    dv[nxt] = dv[i] * 2;
                 }
             }
             if (i % primes[j] == 0) break;
