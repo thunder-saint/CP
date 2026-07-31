34c709d5d043d8c39f64982ac074e885b0052ef7
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-08-01 01:50:29 +0600

    Update Topological Sorting.cpp

--- Graph Theory/Topological Sorting.cpp
+++ Graph Theory/Topological Sorting.cpp
@@ -1,18 +1,19 @@
 const int N = 1e5 + 1;
-int indeg[N];
 vector<int> g[N];
+int indeg[N], ways[N]; 
 vector<int> Topological_sort(int n) {
     vector<int> ans;
     queue<int> q;
     for (int i = 1; i <= n; i++) {
+        ways[i] = 1; 
         if (indeg[i] == 0) q.push(i);
     }
     while (!q.empty()) {
         int cur = q.front();
         q.pop();
         ans.push_back(cur);
-
         for (auto v : g[cur]) {
+            ways[v] += ways[cur];
             if (--indeg[v] == 0) q.push(v);
         }
     }
