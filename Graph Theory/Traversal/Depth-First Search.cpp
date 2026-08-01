55aa90edae55a25997104e3ea3345e61008d6fea
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-08-01 15:34:27 +0600

    Update Depth-First Search.cpp

--- Graph Theory/Traversal/Depth-First Search.cpp
+++ Graph Theory/Traversal/Depth-First Search.cpp
@@ -1,16 +1,19 @@
 const int N = 1e5 + 1;
 vector<int> g[N];
+int up[N][30];
 bool vis[N], leaf[N];
 int depth[N], subtree_sz[N], height[N];
 int tin[N], tout[N], par[N];
 int timer = 0;
 void dfs(int u, int p ) {
     tin[u] = ++timer; 
-    vis[u] = true;
+    vis[u] = leaf[u] = true;
     height[u] = 0;  
     subtree_sz[u] = 1;
-    par[u] = p;
-    leaf[u] = true;
+    par[u] = up[u][0] = p;
+    for (int i = 1; i < 30; i++) {
+            up[u][i] = up[up[u][i - 1]][i - 1];
+        }
     for (auto &v : g[u]) {
         if(v == p) continue;
         if (!vis[v]) {
