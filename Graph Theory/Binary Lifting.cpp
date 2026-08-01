ecf19653d08179a1fcf8c1ad8acfbe7cdda9d5ec
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-08-01 16:07:14 +0600

    Create  Binary Lifting.cpp

---
+++ Graph Theory/Binary Lifting.cpp
@@ -0,0 +1,32 @@
+struct BinaryLifting {
+    bool ancestor(int u, int v) {
+        return tin[u] <= tin[v] && tout[u] >= tout[v];
+    }
+    int lca(int u, int v) {
+        if (ancestor(u, v)) return u;
+        if (ancestor(v, u)) return v;
+        for (int i = LOG - 1; i >= 0; i--) {
+            if (up[u][i] != 0 && !ancestor(up[u][i], v)) 
+                u = up[u][i];
+        }
+        return up[u][0];
+    }
+    int dist(int u, int v) {
+        return depth[u] + depth[v] - 2 * depth[lca(u, v)];
+    }
+    int kth(int u, int k) {
+        for (int i = 0; i < LOG; i++) {
+            if ((k >> i) & 1) u = up[u][i];
+            if (u == 0) return 0;
+        }
+        return u;
+    }
+    int k_node(int u, int v, int k) {
+        int l = lca(u, v);
+        int d = depth[u] + depth[v] - (depth[l] << 1);
+        if (k > d) return -1; 
+        if (depth[l] + k <= depth[u]) return kth(u, k);
+        k -= depth[u] - depth[l];
+        return kth(v, depth[v] - depth[l] - k);
+    }
+};
