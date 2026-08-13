919d92c79e6d17415479ed9b86234913317e60b9
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-08-14 00:04:18 +0600

    Update Depth-First Search.cpp

--- Graph Theory/Traversal/Depth-First Search.cpp
+++ Graph Theory/Traversal/Depth-First Search.cpp
@@ -1,26 +1,45 @@
 const int N = 1e5 + 1;
-vector<int> g[N];
 const int LOG = 31;
+vector<int> g[N], stk;
+vector<vector<int>> sccs;
 int up[N][LOG], par[N];
-bool vis[N], leaf[N];
+bool vis[N], leaf[N], in_stk[N];
 int depth[N], subtree_sz[N], height[N];
 int tin[N], tout[N], flat_tree[N];
-int timer = 0;
+int low[N], comp_id[N];
+int timer = 0, scc_cnt = 0;
 void dfs(int u, int p ) {
+    tin[u] = low[u] = ++timer;
     flat_tree[timer] = u;
-    tin[u] = timer++; 
-    vis[u] = leaf[u] = true;
     height[u] = 0, subtree_sz[u] = 1;
     par[u] = up[u][0] = p;
+    vis[u] = leaf[u] = in_stk[u] = true;
+    stk.push_back(u);
     for (auto &v : g[u]) {
-        if(v == p) continue;
         if (!vis[v]) {
             leaf[u] = false; 
             depth[v] = depth[u] + 1;
             dfs(v,u);
             height[u]=max(height[u],height[v]+1);
             subtree_sz[u] += subtree_sz[v];
+            low[u] = min(low[u], low[v]);
+        }
+        else if (in_stk[v]) { 
+            low[u] = min(low[u], tin[v]);
         }
     }
     tout[u] = timer;
+    if (low[u] == tin[u]) {
+        scc_cnt++;
+        vector<int> scc;
+        while (true) {
+            int node = stk.back();
+            stk.pop_back();
+            in_stk[node] = false;
+            comp_id[node] = scc_cnt;
+            scc.push_back(node);
+            if (node == u) break;
+        }
+        sccs.push_back(scc);
+    }
 }
