91d72717ec179c2414ec9f68c716c186938ffbb8
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-08-18 01:59:26 +0600

    Update Depth-First Search.cpp

--- Graph Theory/Traversal/Depth-First Search.cpp
+++ Graph Theory/Traversal/Depth-First Search.cpp
@@ -1,47 +1,77 @@
 const int N = 1e5 + 1;
 const int LOG = 31;
 vector<int> g[N], stk;
-vector<vector<int>> sccs;
+vector<pair<int, int>> edge_stk;
+vector<vector<int>> comps, bccs;
 int up[N][LOG], par[N];
-bool vis[N], leaf[N], in_stk[N];
+bool vis[N], leaf[N], in_stk[N], cutpoint[N];
 int depth[N], subtree_sz[N], height[N];
 int tin[N], tout[N], flat_tree[N];
 int low[N], comp_id[N];
-int timer = 0, comp_cnt = 0;
-bool directed = false;
-void dfs(int u, int p ) {
+int timer = 0, comp_cnt = 0, bcc_cnt = 0;
+void dfs(int u, int p) {
     tin[u] = low[u] = ++timer;
     flat_tree[timer] = u;
     height[u] = 0, subtree_sz[u] = 1;
     par[u] = up[u][0] = p;
     vis[u] = leaf[u] = in_stk[u] = true;
     stk.push_back(u);
+    int child = 0; 
     for (auto &v : g[u]) {
-        if (!directed && v == p) continue;
+        if (v == p) continue; // BCC && 2ECC
+        // use egde id for multiple edges
         if (!vis[v]) {
+            edge_stk.push_back({u, v}); // BCC
             leaf[u] = false; 
             depth[v] = depth[u] + 1;
-            dfs(v,u);
-            height[u]=max(height[u],height[v]+1);
+            child++;
+            dfs(v, u);
+            height[u] = max(height[u], height[v] + 1);
             subtree_sz[u] += subtree_sz[v];
             low[u] = min(low[u], low[v]);
+
+            // --- BCC---
+            if (low[v] >= tin[u]) {
+                if (p != 0) cutpoint[u] = true;
+                bcc_cnt++;
+                vector<int> bcc;
+                while (true) {
+                    auto [f, s] = edge_stk.back();
+                    edge_stk.pop_back();
+                    bcc.push_back(f);
+                    bcc.push_back(s);
+                    if (f == u && s == v) break;
+                }
+                sort(all(bcc));
+                bcc.erase(unique(all(bcc)), bcc.end());
+                bccs.push_back(bcc);
+            }
+            // ----------------------------------------
         }
         else if (in_stk[v]) { 
+            // BCC
+            if (tin[v] < tin[u]) {
+                edge_stk.push_back({u, v});
+            }
+            // all
             low[u] = min(low[u], tin[v]);
         }
     }
+    if (p == 0 && child > 1) cutpoint[u] = true; //BCC
     tout[u] = timer;
+    
+    // --- SCC && 2ECC ---
     if (low[u] == tin[u]) {
         comp_cnt++;
-        vector<int> scc;
+        vector<int> comp;
         while (true) {
             int node = stk.back();
             stk.pop_back();
             in_stk[node] = false;
             comp_id[node] = comp_cnt;
-            scc.push_back(node);
+            comp.push_back(node);
             if (node == u) break;
         }
-        sccs.push_back(scc);
+        comps.push_back(comp);
     }
 }
