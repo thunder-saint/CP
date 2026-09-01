e3c2e26bcd22ff2351e5c3d7c33f19bd8abead2b
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-09-02 04:35:05 +0600

    Fix distance vector size in shortest_path_k_stops
    Adjust the size of the distance vector to accommodate an additional node.

--- Graph Theory/Shortest Path Algorithms/shortest_path_k_stops.cpp
+++ Graph Theory/Shortest Path Algorithms/shortest_path_k_stops.cpp
@@ -3,7 +3,7 @@ vector<tuple<int, int, int>> e;
 vector<long long> d; 
 // excludes src and dst;
 void shortest_path_k_stops(int s, int n, int k) {
-    d.assign(n, INF);
+    d.assign(n + 1, INF);
     d[s] = 0;
     for (int i = 0; i <= k; ++i) {
         vector<long long> temp_d = d; 
