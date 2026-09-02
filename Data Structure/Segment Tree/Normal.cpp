19f8dde8deb8a56839ace39501eb09bba7967e0f
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-09-02 23:22:32 +0600

    Update Normal.cpp

--- Data Structure/Segment Tree/Normal.cpp
+++ Data Structure/Segment Tree/Normal.cpp
@@ -1,16 +1,14 @@
 const int N = 3e5+1;
 int a[N];
-struct ST {
+struct Segment_Tree {
     int t[4 * N];
     static const int neut = LLONG_MIN; // Neutral element (0 for sum, inf for min)
-
-    ST() {
+    Segment_Tree() {
         memset(t,0, sizeof t);
     }
-    inline int merge(int a, int b) {
+    inline int operation(int a, int b) {
         return max(a, b); // Operation
     }
-
     void build(int n, int b, int e) {
         if (b == e) {
             t[n] = a[b];
@@ -19,25 +17,23 @@ struct ST {
         int mid = (b + e) >> 1, l = n << 1, r = l | 1;
         build(l, b, mid);
         build(r, mid + 1, e);
-        t[n] = merge(t[l], t[r]);
+        t[n] = operation(t[l], t[r]);
     }
-
-    void upd(int n, int b, int e, int i, int x) {
+    void update(int n, int b, int e, int i, int x) {
         if (b > i || e < i) return;
         if (b == e && b == i) {
             t[n] = x;
             return;
         }
         int mid = (b + e) >> 1, l = n << 1, r = l | 1;
-        upd(l, b, mid, i, x);
-        upd(r, mid + 1, e, i, x);
-        t[n] = merge(t[l], t[r]);
+        update(l, b, mid, i, x);
+        update(r, mid + 1, e, i, x);
+        t[n] = operation[l], t[r]);
     }
-
     int query(int n, int b, int e, int i, int j) {
         if (b > j || e < i) return neut;
         if (b >= i && e <= j) return t[n];
         int mid = (b + e) >> 1, l = n << 1, r = l | 1;
-        return merge(query(l, b, mid, i, j), query(r, mid + 1, e, i, j));
+        return operation(query(l, b, mid, i, j), query(r, mid + 1, e, i, j));
     }
-} tr;
+};
