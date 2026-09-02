7492db40aa7dcfc51d80c0dfb10b774863e81945
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-09-03 05:03:05 +0600

    Refactor Segment Tree implementation
    Refactor segment tree structure and methods for clarity and consistency.

--- Data Structure/Segment Tree/Lazy Propagation.cpp
+++ Data Structure/Segment Tree/Lazy Propagation.cpp
@@ -1,16 +1,15 @@
-
 const int N = 5e5 + 9;
 int a[N];
-
-struct ST {
-  #define lc (n << 1)
-  #define rc ((n << 1) + 1)
-  long long t[4 * N], lazy[4 * N];
-  ST() {
-    memset(t, 0, sizeof t);
-    memset(lazy, 0, sizeof lazy);
-  }
-  inline void push(int n, int b, int e) { //change
+struct Segment_Tree {
+  #define lc (n << 1LL)
+  #define rc ((n << 1LL) | 1LL)
+  static const int neut = 0; // <--- CHANGE
+  vector<int> t, lazy; // <--- CHANGE
+    Segment_Tree(int n) {
+        t.assign(4 * n, 0);
+        lazy.assign(4 * n, 0);
+    }
+  inline void push(int n, int b, int e) { // <--- CHANGE
     if (lazy[n] == 0) return;
     t[n] = t[n] + lazy[n] * (e - b + 1);
     if (b != e) {
@@ -19,16 +18,16 @@ struct ST {
     }
     lazy[n] = 0;
   }
-  inline long long combine(long long a,long long b) {
-    return a + b; // change
+  inline int merge(int a,int b) {
+    return a + b; // <--- CHANGE
   }
   inline void pull(int n) {
-    t[n] = combine(t[lc], t[rc]);
+    t[n] = merge(t[lc], t[rc]); // <--- CHANGE
   }
   void build(int n, int b, int e) {
-    lazy[n] = 0; // change
+    lazy[n] = 0; // <--- CHANGE
     if (b == e) {
-      t[n] = a[b];
+      t[n] = a[b]; // <--- CHANGE
       return;
     }
     int mid = (b + e) >> 1;
@@ -36,24 +35,24 @@ struct ST {
     build(rc, mid + 1, e);
     pull(n);
   }
-  void upd(int n, int b, int e, int i, int j, long long v) {
+  void update(int n, int b, int e, int i, int j, int v) {
     push(n, b, e);
     if (j < b || e < i) return;
     if (i <= b && e <= j) {
-      lazy[n] += v;
+      lazy[n] += v; // <--- CHANGE
       push(n, b, e);
       return;
     }
     int mid = (b + e) >> 1;
-    upd(lc, b, mid, i, j, v);
-    upd(rc, mid + 1, e, i, j, v);
+    update(lc, b, mid, i, j, v);
+    update(rc, mid + 1, e, i, j, v);
     pull(n);
   }
-  long long query(int n, int b, int e, int i, int j) {
+  int query(int n, int b, int e, int i, int j) {
     push(n, b, e);
-    if (i > e || b > j) return 0;
+    if (i > e || b > j) return neut; // <--- CHANGE
     if (i <= b && e <= j) return t[n];
     int mid = (b + e) >> 1;
-    return combine(query(lc, b, mid, i, j), query(rc, mid + 1, e, i, j));
+    return merge(query(lc, b, mid, i, j), query(rc, mid + 1, e, i, j));
   }
-} tr;
+};
