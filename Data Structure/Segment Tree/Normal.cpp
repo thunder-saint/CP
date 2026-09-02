ff2740f2aaf26d434623f034a5f7d894036f783c
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-09-02 23:34:35 +0600

    Update Normal.cpp

--- Data Structure/Segment Tree/Normal.cpp
+++ Data Structure/Segment Tree/Normal.cpp
@@ -6,7 +6,7 @@ struct Segment_Tree {
     Segment_Tree() {
         memset(t,0, sizeof t);
     }
-    inline int operation(int a, int b) {
+    inline int merge(int a, int b) {
         return max(a, b); // Operation
     }
     void build(int n, int b, int e) {
@@ -17,7 +17,7 @@ struct Segment_Tree {
         int mid = (b + e) >> 1, l = n << 1, r = l | 1;
         build(l, b, mid);
         build(r, mid + 1, e);
-        t[n] = operation(t[l], t[r]);
+        t[n] = merge(t[l], t[r]);
     }
     void update(int n, int b, int e, int i, int x) {
         if (b > i || e < i) return;
@@ -28,12 +28,12 @@ struct Segment_Tree {
         int mid = (b + e) >> 1, l = n << 1, r = l | 1;
         update(l, b, mid, i, x);
         update(r, mid + 1, e, i, x);
-        t[n] = operation([l], t[r]);
+        t[n] = merge([l], t[r]);
     }
     int query(int n, int b, int e, int i, int j) {
         if (b > j || e < i) return neut;
         if (b >= i && e <= j) return t[n];
         int mid = (b + e) >> 1, l = n << 1, r = l | 1;
-        return operation(query(l, b, mid, i, j), query(r, mid + 1, e, i, j));
+        return merge(query(l, b, mid, i, j), query(r, mid + 1, e, i, j));
     }
 };
