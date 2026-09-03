30ce084e74ac889a34ef9340d43660457374f34c
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-09-04 00:03:12 +0600

    Update and rename coordinate compression.cpp to coordinate compression.cpp

--- Data Structure/coordinate compression.cpp
+++ Data Structure/coordinate compression.cpp
@@ -1,9 +1,7 @@
 vector<int> compress(vector<int> &a) {
     vector<int> v = a;
-
     sort(v.begin(), v.end());
     v.erase(unique(v.begin(), v.end()), v.end());
-
     for (int i = 0; i < a.size(); i++) {
         a[i] = lower_bound(v.begin(), v.end(), a[i]) - v.begin() + 1;
     }
