7363263a9de95c3304d37820d849f3baa653983e
Ahnaf Shahriar <ahnafshahriar418@gmail.com>
2026-09-04 20:29:05 +0600

    Refactor segment tree constants and update logic

--- Data Structure/Segment Tree/Lazy Propagation.cpp
+++ Data Structure/Segment Tree/Lazy Propagation.cpp
@@ -3,9 +3,9 @@ int a[N];
 struct Segment_Tree {
 #define lc (n << 1LL)
 #define rc ((n << 1LL) | 1LL)
-    static const int neut_query = 0; // <--- CHANGE 
-    static const int neut_lazy = 0;  // <--- CHANGE 
-    vector<int> t, lazy; // <--- CHANGE
+    static const int neut_query = 0; // CHANGE 
+    static const int neut_lazy = 0;  //  CHANGE 
+    vector<int> t, lazy; // CHANGE
     vector<bool> is_lazy;
     Segment_Tree(int n) {
         t.assign(4 * n, 0);
@@ -14,16 +14,16 @@ struct Segment_Tree {
     }
     inline void push(int n, int b, int e) { 
         if (!is_lazy[n]) return;   
-        t[n] += lazy[n] * (e - b + 1); // <--- CHANGE 
+        t[n] += lazy[n] * (e - b + 1); // CHANGE 
         if (b != e) {
-            lazy[lc] += lazy[n], lazy[rc] += lazy[n]; // <--- CHANGE
+            lazy[lc] += lazy[n], lazy[rc] += lazy[n]; // CHANGE
             is_lazy[lc] = is_lazy[rc] = true;
         }
         lazy[n] = neut_lazy;
         is_lazy[n] = false;
     }
     inline int merge(int a, int b) {
-        return a + b; // <--- CHANGE
+        return a + b; // CHANGE
     }
     inline void pull(int n) {
         t[n] = merge(t[lc], t[rc]); 
@@ -32,7 +32,7 @@ struct Segment_Tree {
         lazy[n] = neut_lazy; 
         is_lazy[n] = false;
         if (b == e) {
-            t[n] = a[b]; // <--- CHANGE 
+            t[n] = a[b]; // CHANGE 
             return;
         }
         int mid = (b + e) >> 1;
@@ -44,7 +44,7 @@ struct Segment_Tree {
         push(n, b, e);
         if (j < b || e < i) return;
         if (i <= b && e <= j) {
-            lazy[n] += v; // <--- CHANGE
+            lazy[n] += v; // CHANGE
             is_lazy[n] = true;
             push(n, b, e);
             return;
@@ -56,7 +56,7 @@ struct Segment_Tree {
     }
     int query(int n, int b, int e, int i, int j) {
         push(n, b, e);
-        if (i > e || b > j) return neut_query; // <--- CHANGE
+        if (i > e || b > j) return neut_query; // CHANGE
         if (i <= b && e <= j) return t[n];
         int mid = (b + e) >> 1;
         int q1 = query(lc, b, mid, i, j);
